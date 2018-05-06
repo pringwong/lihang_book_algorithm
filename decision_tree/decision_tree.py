20af68fa1bc87ad70a24820e7c28b45fa6a4a452
Pring Wong <huppid@qq.com>
2018-05-06 22:24:37 +0800

    Update decision_tree.py
    简化set

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -72,7 +72,7 @@ def calc_ent(x):
         calculate shanno ent of x
     """
 
-    x_value_list = set([x[i] for i in range(x.shape[0])])
+    x_value_list = set(x)
     ent = 0.0
     for x_value in x_value_list:
         p = float(x[x == x_value].shape[0]) / x.shape[0]
@@ -87,7 +87,7 @@ def calc_condition_ent(x, y):
     """
 
     # calc ent(y|x)
-    x_value_list = set([x[i] for i in range(x.shape[0])])
+    x_value_list = set(x)
     ent = 0.0
     for x_value in x_value_list:
         sub_y = y[x == x_value]
